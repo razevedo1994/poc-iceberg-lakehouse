eb1f9b3ce5cdc4c9d02f2ab7fb99efffb5d2270e
Rodrigo <razevedo.contato@gmail.com>
2025-08-22 19:13:52 -0300

    feat: Creates a bucket creation statement

---
+++ storage/s3.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name = "lakehouse-storage"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+    bucket = aws_s3_bucket.this.id
+
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+}
